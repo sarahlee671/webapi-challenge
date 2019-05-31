a25f471b033fa32d9afc4460b5ae9605addac41b
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-05-31 12:28:33 -0700

    added delete for actions

--- router.js
+++ router.js
@@ -88,5 +88,51 @@ router.post('/:id/actions', async(req,res) => {
     }
 })
 
+router.put('/:id/actions', async(req,res) => {
+    try {
+        const action = await actionData.update(req.params.id, req.body)
+        if (action) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: "null" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "error updating project"});
+    }    
+})
+
+// router.delete('/:id/actions/:id', (req, res) => {
+//         const id = req.params.id;
+//         projectData
+//             .remove(id)
+//             .then(id => {
+//                 if (id.length) {
+//                     res.status(404).json({message: "The action with the specified ID does not exist"})
+//                 } else {
+//                     res.status(200).json({ message: "successfully removed"})
+//                 }
+//             })
+//             .catch(error => {
+//                 res.status(500).json({error: "The action could not be removed"})
+//             })
+// })
+
+router.delete('/:id/actions/:id', async (req, res) => {
+    try {
+        const count = await actionData.remove(req.params.id);
+        if (count > 0) {
+          res.status(200).json({ message: `action has been removed`});
+        } else {
+          res.status(404).json({ message: 'The action could not be found' });
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error removing the project',
+        });
+      }
+    });
+
 
 module.exports = router;
